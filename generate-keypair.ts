4ef4fbc60ff42561366025dbd35134c43eb36f2a
BrightStar1001 <mitrodev23@gmail.com>
2024-04-29 01:47:26 -0700

    generate KeyPair & loading an existing keypair

---
+++ generate-keypair.ts
@@ -0,0 +1,17 @@
+// import { Keypair } from "@solana/web3.js";
+
+// const keypair = Keypair.generate();
+
+// console.log(`The public key is: `, keypair.publicKey.toBase58());
+// console.log(`The secret key is: `, keypair.secretKey);
+// console.log(`✅ Finished!`);
+
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+console.log(keypair.publicKey.toBase58());
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
